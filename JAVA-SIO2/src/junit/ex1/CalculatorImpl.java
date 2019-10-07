64e6731746e33e6f68d425a416160d93e1d9a893
ekrod-prof <ekrod.daker@netcourrier.com>
2019-10-07 17:16:07 +0200

    *

--- JAVA-SIO2/src/junit/ex1/CalculatorImpl.java
+++ JAVA-SIO2/src/junit/ex1/CalculatorImpl.java
@@ -4,9 +4,79 @@ public class CalculatorImpl implements Calculator{
 	//constructeur
 	CalculatorImpl(){};
 	@Override
-	public int multiply(int a, int b) {return 0;};
+	public int multiply(int a, int b) 
+	{
+		int res=0;int a1=0;int b1=0;
+		if (a==0 | b==0) {res=0;}
+		else 
+			{			
+				if (a<0) {a1=-a;}
+				else {a1=a;}
+				if (b<0) {b1=-b;}
+				else {b1=b;}
+				
+							
+				while (b1>0) 
+				{
+					res=res+add(a1,0);
+					b1--;
+				}
+				//if (a>0 && b>0) {return res=res;}
+				//if (a<0 | b<0) {return res=res;}
+				if ((a>0 && b<0) | (a<0 && b>0)) {res=res*(-1);}
+			
+			}
+		return res;	
+	}
+	
 	@Override
-	public int divide(int a, int b){return 0;};
+	public int divide(int a, int b)
+	{
+		int res=0;int a1=0;int b1=0;int i=0;
+		if (b==0) 
+		{
+			System.out.println("Erreur Division par zero.");
+			res=010101;
+		}
+		if (a==0 && b!=0) {res=0;}
+		if (a<0) {a1=-a;}
+		else {a1=a;}
+		if (b<0) {b1=-b;}
+		else {b1=b;}
+		//System.out.println("a1"+a1+"  b1 "+b1);
+		for (i=1;i<=a1;i++)
+		{
+			if (multiply(i,b1)>=a1)
+			{
+				if (multiply(i,b1)>a1)	
+				{
+					i=i-1;
+					res=i;
+					//res=a1+"="+i+"x"+b1+substract(a1,multiply(i,b1));
+					System.out.println("** "+i+" "+a1/b1);
+					break;
+				}
+				else
+				{
+					res=i;
+					//res=a1+"="+i+"x"+b1+substract(a1,multiply(i,b1));
+					System.out.println("zz "+i+" "+a1/b1);
+					break;
+				}
+			}
+			System.out.println("for "+i+" "+a1/b1);
+		} //end for
+		if ((a>0 && b<0) | (a<0 && b>0)) {res=res*(-1);}
+	System.out.println("ppp "+i+" res="+res+"  "+a1/b1);
+	return res;
+	}
+		
+		
+	
+	
+	
+	
+	
 	
 	@Override
 	public int add(int a, int b) {
--- JAVA-SIO2/src/junit/ex1/CalculatorTest.java
+++ JAVA-SIO2/src/junit/ex1/CalculatorTest.java
@@ -5,7 +5,46 @@ import org.junit.jupiter.api.Test;
 class CalculatorTest {
 	
 		@Test
-		public final void testDivide() {}//TODO
+		public final void testDivide()
+		{
+			Calculator calc = new CalculatorImpl();
+			int a, b, res;
+			a = 25; 
+		    b  = 3;
+		    res = a/b;
+			if (calc.divide(a,b) != res) 
+			{
+				fail("FAIL");
+			}
+			a = -25; 
+		    b  = 3;
+		    res = a/b;
+			if (calc.divide(a,b) != res) 
+			{
+				fail("FAIL");
+			}	
+			a = 25; 
+		    b  = -3;
+		    res = a/b;
+			if (calc.divide(a,b) != res) 
+			{
+				fail("FAIL");
+			}	
+			a = -25; 
+		    b  = -3;
+		    res = a/b;
+			if (calc.divide(a,b) != res) 
+			{
+				fail("FAIL");
+			}		
+			a = 0; 
+		    b  = -3;
+		    res = a/b;
+			if (calc.divide(a,b) != res) 
+			{
+				fail("FAIL");
+			}	
+		}
 		
 	
 
@@ -16,7 +55,7 @@ class CalculatorTest {
 			a = 5; 
 		    b  = 5;
 		    res = a + b;
-			if (calc.add(a, b) != res) {
+			if (calc.add(a,b) != res) {
 				fail("a et b positif");
 				//Assertions.fail("a et b positif");
 				
@@ -30,7 +69,7 @@ class CalculatorTest {
 			a = 5; 
 		    b  = 5;
 		    res = a - b;
-			if (calc.substract(a, b) != res) {
+			if (calc.substract(a,b) != res) {
 				fail("a et b positif");
 		        }
 			a = 0; 
@@ -48,7 +87,7 @@ class CalculatorTest {
 			a = -5; 
 		    b  = 0;
 		    res = a - b;
-			if (calc.substract(a, b) != res) {
+			if (calc.substract(a,b) != res) {
 				fail("a négatif et b=0");
 		        }
 			a = 0; 
@@ -60,26 +99,93 @@ class CalculatorTest {
 			a = -5; 
 		    b  = -5;
 		    res = a - b;
-			if (calc.substract(a, b) != res) {
+			if (calc.substract(a,b) != res) {
 				fail("a et b négatif");
 		        }
 			a = 10; 
 		    b  = -5;
 		    res = a - b;
-			if (calc.substract(a, b) != res) {
+			if (calc.substract(a,b) != res) {
 				fail("a positif et b ,négatif");
 		        }
 			a = -10; 
 		    b  = 5;
 		    res = a - b;
-			if (calc.substract(a, b) != res) {
+			if (calc.substract(a,b) != res) {
 				fail("a négatif et b positif");
 		        }			
 		}
 		
 		@Test
-		public final void testMultiply() {}//TODO
+		public final void testMultiply() 
+		{
+			Calculator calc = new CalculatorImpl();
+			int a, b, res;
+			a = 5; 
+		    b  = 5;
+		    res = a*b;
+			if (calc.multiply(a,b) != res) 
+			{
+				fail("a et b positif");
+			}
+			
+			
+			a = -5; 
+		    b  = 5;
+		    res = a*b;
+			if (calc.multiply(a,b) != res) 
+			{
+				fail("a negatif et b positif");
+			}
+			a = 5; 
+		    b  = -5;
+		    res = a*b;
+			if (calc.multiply(a,b) != res) 
+			{
+				fail("a positif et b negatif");
+			}
+			
+			a = 0; 
+		    b  = 5;
+		    res = a*b;
+			if (calc.multiply(a,b) != res) 
+			{
+				fail("a=0 et b positif");
+			}
+			a = 0; 
+		    b  = -5;
+		    res = a*b;
+			if (calc.multiply(a,b) != res) 
+			{
+				fail("a=0 et b negatif");
+			}
+			
+			a = 5; 
+		    b  = 0;
+		    res = a*b;
+			if (calc.multiply(a,b) != res) 
+			{
+				fail("a positif et b=0");
+			}
+			a = -5; 
+		    b  = 0;
+		    res = a*b;
+			if (calc.multiply(a,b) != res) 
+			{
+				fail("a negatif et b=0");
+			}
+			
+			a = 0; 
+		    b  = 0;
+		    res = a*b;
+			if (calc.multiply(a,b) != res) 
+			{
+				fail("a=0 et b=0");
+			}
+
+		}
+}
 			
 		
-
-}
+			
+		
